7fe4e113617505f18483a6e6d1b2f62f506e9a1a
Takeo Shimazu <takeo2039@gmail.com>
2026-06-11 17:19:20 -0700

    restore old files and delet bing images

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 //include Express
 const express = require('express');
 
-// configure port number
+//server will listen on this port
 const port = 3000;
-
-// 💡 CRITICAL: Load our user data file from the data folder
-const userData = require('./data/test.json');
 
 //create instance of Express app
 const app = express();
@@ -18,49 +15,14 @@ app.use(express.static(__dirname + '/public'));
 
 //index/home URL
 app.get('/',(req,res)=>{
-    let title = "About Bing Images";
+    let title = "Home Page";
     res.render('pages/index',{'title': title});
 });
 
-//elephant URL
-app.get('/elephant',(req,res)=>{
-    let title = "about Elephant";
-    res.render('pages/elephant',{'title': title});
-});
-
-//catsle URL
-app.get('/castle',(req,res)=>{
-    let title = "about Catsle";
-    res.render('pages/castle',{'title': title});
-});
-
-//seals URL
-app.get('/seals',(req,res)=>{
-    let title = "about Seals";
-    res.render('pages/seals',{'title': title});
-});
-
-// ==========================================
-// 👥 NEW ROUTE: The User Directory (The List)
-// ==========================================
-app.get('/users', (req, res) => {
-    res.render('users/index', {
-        title: 'User Directory',
-        users: userData // Passes the 100-user array to the template
-    });
-});
-
-// ==========================================
-// 🔍 NEW ROUTE: Individual Profile (The View)
-// ==========================================
-app.get('/users/view/:id', (req, res) => {
-    let id = req.params.id;
-
-    // We cheat elegantly by subtracting 1 to match array zero-indexing
-    res.render('users/view', {
-        title: 'User Profile',
-        user: userData[--id]
-    });
+//about URL
+app.get('/about',(req,res)=>{
+    let title = "About Page";
+    res.render('pages/about',{'title': title});
 });
 
 //Set server to listen for requests
